b3a20d94b40e714aa69c5c09b339d8d60e5358aa
GeoCosta017 <geovannacostag3@gmail.com>
2025-08-12 16:47:16 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um número:'))
+
+print(f'o antecessor de {n1} é {n1-1} e seu sucessor é {n1+1}')
---
+++ indexy.py
@@ -0,0 +1,42 @@
+# + ADIÇÃO
+# - MENOS
+# * MULTIPLICAÇÃO
+# / DIVISÃO
+# // DIVISÃO INTEIRA 
+# % (RESTO DA DIVISÃO)
+# ** EXPONENCIAÇÃO
+
+
+# * MULTIPLICAÇÃO
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+
+input(f'A multiplicação de {n1} e {n2} é {n1*n2}')
+
+
+# / DIVISÃO
+n1 = int(float('Digite um número:'))
+n2 = int(float('Digite outro número:'))
+
+input(f'A divisão de {n1} e {n2} é {n1/n2}')
+
+
+# // DIVISÃO INTEIRA
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+
+input(f'A divisão inteira de {n1} e {n2} é {n1//n2}')
+
+
+# % (RESTO DA DIVISÃO)
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+
+input(f'o resto da divisão de {n1} e {n2} é {n1%n2}')
+
+
+# ** EXPONENCIAÇÃO
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+
+input(f'A divisão inteira de {n1} e {n2} é {n1**n2}')
